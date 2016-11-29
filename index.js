73d1390b3bf0d449f78c0a52c3b0e8eb4cfbca1c
Alessandro <a.romanino@gmial.com>
2016-11-29 10:45:50 +0100

    install on the fly save big fixed in paras list

--- index.js
+++ index.js
@@ -131,7 +131,12 @@ exports.install=function(app,extender,save){
                 functionToString += ("      \"resource\":\"") + obj.resource.toString() + "\",\n";
                 functionToString += ("      \"method\":\"") + obj.method.toString() + "\",\n";
                 functionToString += ("      \"mode\":\"") + obj.mode.toString() + "\",\n";
-                functionToString += ("      \"params\":[") + obj.params.toString() + "],\n";
+                functionToString +=  "      \"params\":[" ;
+                obj.params.forEach(function (value) {
+                    functionToString+="\"" + value.toString() + "\"";
+                });
+                functionToString+="],\n";
+
                 if(obj.extender && obj.extender.before) {
                     functionToString += ("      \"extender\":{\n");
                     functionToString += ("          \"before\":") + obj.extender.before.toString() + ",\n";
